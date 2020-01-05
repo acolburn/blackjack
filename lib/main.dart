4340743a634844dfc2e95b7d2650e6d25603aae0
acolburn <acolburn3@gmail.com>
2020-01-05 12:07:40 -0800

    Added full screen gesture detection

--- lib/main.dart
+++ lib/main.dart
@@ -16,93 +16,100 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHome extends StatelessWidget {
-  var random = Random();
+  final random = Random();
   @override
   Widget build(BuildContext context) {
     List<PlayingCard> deck = makeDeck();
 //    deck.forEach((element) => print('${element.type} of ${element.suit}'));
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Stack(
-          overflow: Overflow.visible,
-          children: <Widget>[
-            buildFaceDownCard(),
-            Positioned(
-              left: 30.0,
-              child: buildCard(deck[random.nextInt(deck.length)]),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            buildCard(deck[random.nextInt(deck.length)]),
-            buildCard(deck[random.nextInt(deck.length)]),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Material(
-              elevation: 5,
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(50),
-              child: MaterialButton(
-                onPressed: null,
-                height: 90,
-                minWidth: 90,
-                child: Text(
-                  'Hit',
-                  style: TextStyle(color: Colors.white),
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent, //entire screen now recognized
+      onTap: () => print('Tapped'),
+      onDoubleTap: () => print('Doubled Tapped'),
+      onVerticalDragEnd: (e) => print('Vertical Swipe'),
+      onHorizontalDragEnd: (e) => print('Horizontal Swipe'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              buildFaceDownCard(),
+              Positioned(
+                left: 30.0,
+                child: buildCard(deck[random.nextInt(deck.length)]),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              buildCard(deck[random.nextInt(deck.length)]),
+              buildCard(deck[random.nextInt(deck.length)]),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Material(
+                elevation: 5,
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(50),
+                child: MaterialButton(
+                  onPressed: null,
+                  height: 90,
+                  minWidth: 90,
+                  child: Text(
+                    'Hit',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            Material(
-              elevation: 5,
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(50),
-              child: MaterialButton(
-                onPressed: null,
-                height: 90,
-                minWidth: 90,
-                child: Text(
-                  'Stand',
-                  style: TextStyle(color: Colors.white),
+              Material(
+                elevation: 5,
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(50),
+                child: MaterialButton(
+                  onPressed: null,
+                  height: 90,
+                  minWidth: 90,
+                  child: Text(
+                    'Stand',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            Material(
-              elevation: 5,
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(50),
-              child: MaterialButton(
-                onPressed: null,
-                height: 90,
-                minWidth: 90,
-                child: Text(
-                  'Double',
-                  style: TextStyle(color: Colors.white),
+              Material(
+                elevation: 5,
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(50),
+                child: MaterialButton(
+                  onPressed: null,
+                  height: 90,
+                  minWidth: 90,
+                  child: Text(
+                    'Double',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            Material(
-              elevation: 5,
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(50),
-              child: MaterialButton(
-                onPressed: null,
-                height: 90,
-                minWidth: 90,
-                child: Text(
-                  'Split',
-                  style: TextStyle(color: Colors.white),
+              Material(
+                elevation: 5,
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(50),
+                child: MaterialButton(
+                  onPressed: null,
+                  height: 90,
+                  minWidth: 90,
+                  child: Text(
+                    'Split',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
 
 //    return Container();
