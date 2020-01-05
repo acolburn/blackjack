d59c54537dbe8935fb6b8bd10d7db1ad06756a15
acolburn <acolburn3@gmail.com>
2020-01-05 11:07:39 -0800

    added hit, stand, etc. buttons

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,67 @@ class MyHome extends StatelessWidget {
             buildCard(deck[random.nextInt(deck.length)]),
           ],
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Material(
+              elevation: 5,
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(50),
+              child: MaterialButton(
+                onPressed: null,
+                height: 90,
+                minWidth: 90,
+                child: Text(
+                  'Hit',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Material(
+              elevation: 5,
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(50),
+              child: MaterialButton(
+                onPressed: null,
+                height: 90,
+                minWidth: 90,
+                child: Text(
+                  'Stand',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Material(
+              elevation: 5,
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(50),
+              child: MaterialButton(
+                onPressed: null,
+                height: 90,
+                minWidth: 90,
+                child: Text(
+                  'Double',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Material(
+              elevation: 5,
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(50),
+              child: MaterialButton(
+                onPressed: null,
+                height: 90,
+                minWidth: 90,
+                child: Text(
+                  'Split',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ],
     );
 
