51fb547f396b798cee75ba6935dc53b490245671
acolburn <acolburn3@gmail.com>
2020-01-06 17:38:36 -0800

    Made Home() stateless, implemented dealing hand by tapping

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
   final random = Random();
+
+  PlayingCard playerCard1;
+
+  PlayingCard playerCard2;
+
+  PlayingCard dealerCard;
+
+  void dealHand() {
+    List<PlayingCard> deck = makeDeck();
+    playerCard1 = deck[random.nextInt(deck.length)];
+    playerCard2 = deck[random.nextInt(deck.length)];
+//    PlayingCard playerCard2 = playerCard1; (for pair testing)
+    dealerCard = deck[random.nextInt(deck.length)];
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<PlayingCard> deck = makeDeck();
-    PlayingCard playerCard1 = deck[random.nextInt(deck.length)];
-    PlayingCard playerCard2 = deck[random.nextInt(deck.length)];
-//    PlayingCard playerCard2 = playerCard1; (pair testing)
-    PlayingCard dealerCard = deck[random.nextInt(deck.length)];
+    dealHand();
     print(testForSplit(playerCard1, playerCard2, dealerCard));
 //    deck.forEach((element) => print('${element.type} of ${element.suit}'));
     return GestureDetector(
       behavior: HitTestBehavior.translucent, //entire screen now recognized
-      onTap: () => print('Hit'),
+      onTap: () {
+        setState(() {
+          dealHand();
+        });
+      },
       onDoubleTap: () => print('Double'),
       onHorizontalDragEnd: (e) => print('Stand'),
       child: Column(
@@ -63,22 +82,42 @@ class MyHome extends StatelessWidget {
               MyButton(
                 buttonText: 'Hit',
                 buttonColor: Colors.green,
-                onPress: () => print('Hit'),
+                onPress: () {
+                  setState(() {
+                    print('Hit');
+                    dealHand();
+                  });
+                },
               ),
               MyButton(
                 buttonText: 'Stand',
                 buttonColor: Colors.red,
-                onPress: () => print('Stand'),
+                onPress: () {
+                  setState(() {
+                    print('Stand');
+                    dealHand();
+                  });
+                },
               ),
               MyButton(
                 buttonText: 'Double',
                 buttonColor: Colors.blue,
-                onPress: () => print('Double'),
+                onPress: () {
+                  setState(() {
+                    print('Double');
+                    dealHand();
+                  });
+                },
               ),
               MyButton(
                 buttonText: 'Split',
                 buttonColor: Colors.amber,
-                onPress: () => print('Split'),
+                onPress: () {
+                  setState(() {
+                    print('Split');
+                    dealHand();
+                  });
+                },
               ),
             ],
           ),
