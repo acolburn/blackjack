e9856186359ca39cf53b86e435878e7855c2485a
acolburn <acolburn3@gmail.com>
2020-02-13 10:43:09 -0800

    Tried (unsuccessfully) to use Provider with count_deck

--- lib/count_deck.dart
+++ lib/count_deck.dart
@@ -2,6 +2,72 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'playing_card.dart';
 import 'info_cell.dart';
+import 'package:provider/provider.dart';
+
+class Data extends ChangeNotifier {
+  int i = 0;
+  int count = 0;
+  bool countIsVisible = true;
+  bool isMultiCard = false;
+  List<PlayingCard> deck = makeDeck();
+
+  PlayingCard currentCard() {
+    return deck[i];
+  }
+
+  bool isEndOfDeck() {
+    if (i == 51) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void changeCountVisibility() {
+    countIsVisible = !countIsVisible;
+    notifyListeners();
+  }
+
+  void changeIsMulticard(bool value) {
+    isMultiCard = value;
+    notifyListeners();
+  }
+
+  void resetDeck() {
+    deck.shuffle();
+
+    i = 0;
+    if (cardValueToNumber(deck[0]) >= 10) {
+      count = -1;
+    } else if (cardValueToNumber(deck[0]) <= 7) {
+      count = 1;
+    } else {
+      count = 0;
+    }
+    notifyListeners();
+  }
+
+  void adjustCount(PlayingCard aCard) {
+    if (cardValueToNumber(aCard) >= 10) {
+      count--;
+    } else if (cardValueToNumber(aCard) <= 7) {
+      count++;
+    }
+    notifyListeners();
+  }
+
+  bool countNextCard() {
+    bool result = true;
+    if (i < 51) {
+      i++;
+      adjustCount(deck[i]);
+    } else {
+      result = false;
+    }
+    notifyListeners();
+    return result;
+  }
+}
 
 class CountDeck extends StatefulWidget {
   @override
@@ -9,20 +75,22 @@ class CountDeck extends StatefulWidget {
 }
 
 class _CountDeckState extends State<CountDeck> {
-  int i = 0;
-  int count = 0;
-  bool isVisible = true;
-  bool isMultiCard = false;
-  List<PlayingCard> deck = makeDeck();
+//  int i = 0;
+//  int count = 0;
+//  bool isVisible = true;
+//  bool isMultiCard = false;
+//  List<PlayingCard> deck = makeDeck();
 
   @override
   void initState() {
     super.initState();
-    deck.shuffle();
+//    Provider.of<Data>(context).deck.shuffle();
   }
 
   @override
   Widget build(BuildContext context) {
+//    final providerOfData = Provider.of<Data>(context);
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -41,42 +109,45 @@ class _CountDeckState extends State<CountDeck> {
                 children: <Widget>[
                   makeInfoCellButton(
                     name: 'Reset Deck',
-                    action: resetDeck,
+                    action: Provider.of<Data>(context).resetDeck,
                   ),
                   makeInfoCellButton(
                       name: 'Display Count',
                       action: () {
                         setState(() {
-                          isVisible = !isVisible;
+                          Provider.of<Data>(context).changeCountVisibility();
                         });
                       }),
                   makeInfoCellButton(
-                      name: 'Auto',
-                      action: () {
-                        resetDeck();
-                        Timer.periodic(Duration(seconds: 1), (timer) {
-                          print('$i');
-                          if (i < 51) {
-                            i++;
-                            if (cardValueToNumber(deck[i]) >= 10) {
-                              count--;
-                            } else if (cardValueToNumber(deck[i]) <= 7) {
-                              count++;
-                            }
-                            setState(() {});
-                          } else
-                            timer.cancel();
-                        });
-                      }),
+                    name: 'Auto',
+                    action: () {
+                      Provider.of<Data>(context).resetDeck();
+                      Timer.periodic(
+                        Duration(seconds: 1),
+                        (timer) {
+                          //while there's still cards in the deck
+                          while (Provider.of<Data>(context).isEndOfDeck() ==
+                              false) {
+                            //get the next card, adjust the count, display the card
+                            Provider.of<Data>(context).countNextCard();
+                          }
+                          //when there's no cards in the deck
+                          //stop the timer
+                          timer.cancel();
+                        },
+                      );
+                    },
+                  ),
                   Row(
                     children: <Widget>[
                       Checkbox(
                         activeColor: Colors.white,
                         checkColor: Colors.green,
-                        value: isMultiCard ?? true,
+                        value: Provider.of<Data>(context).isMultiCard ?? true,
                         onChanged: (bool newValue) {
                           setState(() {
-                            isMultiCard = newValue;
+                            Provider.of<Data>(context)
+                                .changeIsMulticard(newValue);
                           });
                         },
                       ),
@@ -95,20 +166,15 @@ class _CountDeckState extends State<CountDeck> {
             Expanded(flex: 1, child: Container()),
             GestureDetector(
               onTap: () {
-                if (i < 51) {
-                  setState(() {
-                    i++;
-                    adjustCount(deck[i]);
-                  });
-                }
+                Provider.of<Data>(context).countNextCard();
               },
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  buildCard(deck[i]),
+                  buildCard(Provider.of<Data>(context).currentCard()),
                   Visibility(
-                      visible: isVisible,
-                      child: Text('$count',
+                      visible: Provider.of<Data>(context).countIsVisible,
+                      child: Text('${Provider.of<Data>(context).count}',
                           style: TextStyle(fontSize: 42, color: Colors.black)))
                 ],
               ),
@@ -121,26 +187,5 @@ class _CountDeckState extends State<CountDeck> {
         ),
       ),
     );
-  }
-
-  void resetDeck() {
-    deck.shuffle();
-    setState(() {
-      i = 0;
-      if (cardValueToNumber(deck[0]) >= 10) {
-        count = -1;
-      } else if (cardValueToNumber(deck[0]) <= 7) {
-        count = 1;
-      } else
-        count = 0;
-    });
-  }
-
-  void adjustCount(PlayingCard aCard) {
-    if (cardValueToNumber(aCard) >= 10) {
-      count--;
-    } else if (cardValueToNumber(aCard) <= 7) {
-      count++;
-    }
   }
 }
