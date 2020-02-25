1ff335353e9fa975a9e9999ed5212bd59fc9ed0a
acolburn <acolburn3@gmail.com>
2020-02-25 11:19:13 -0800

    Introducing highCountHands

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:provider/provider.dart';
 import 'stopwatch.dart';
 
 enum Decision { hit, stand, double, split, none }
-enum HandType { pairs, softHands, allHands }
+enum HandType { pairs, softHands, allHands, highCountHands }
 
 void main() => runApp(MyApp());
 
@@ -35,106 +35,120 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     HandType hand;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            // color: Colors.black,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/start_screen_image.jpg'),
-                fit: BoxFit.cover,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          // color: Colors.black,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/start_screen_image.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          constraints:
+              BoxConstraints.expand(), //forces container to fill screen
+          //without it, screen's only width of the column
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
               ),
-            ),
-            constraints:
-                BoxConstraints.expand(), //forces container to fill screen
-            //without it, screen's only width of the column
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
+              Text(
+                'Blackjack Trainer',
+                style: TextStyle(
+                    fontSize: 34, fontFamily: 'Verdana', color: Colors.white),
+              ),
+              Text(
+                'What kind of hand would you like to practice?',
+                style: TextStyle(
+                    fontSize: 18, fontFamily: 'Verdana', color: Colors.white),
+              ),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'Soft Hands',
+                  style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
                 ),
-                Text(
-                  'Blackjack Trainer',
-                  style: TextStyle(
-                      fontSize: 34, fontFamily: 'Verdana', color: Colors.white),
+                onPressed: () {
+                  hand = HandType.softHands;
+                  // Navigator.of(context).pushNamed('/home_screen');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => MyHome(hand),
+                    ),
+                  );
+                },
+              ),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'Pairs',
+                  style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
                 ),
-                Text(
-                  'What kind of hand would you like to practice?',
-                  style: TextStyle(
-                      fontSize: 18, fontFamily: 'Verdana', color: Colors.white),
+                onPressed: () {
+                  hand = HandType.pairs;
+                  // Navigator.of(context).pushNamed('/home_screen');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => MyHome(hand),
+                    ),
+                  );
+                },
+              ),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'All Hands',
+                  style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
                 ),
-                FlatButton(
-                  color: Colors.white,
-                  child: Text(
-                    'Soft Hands',
-                    style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
-                  ),
-                  onPressed: () {
-                    hand = HandType.softHands;
-                    // Navigator.of(context).pushNamed('/home_screen');
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => MyHome(hand),
-                      ),
-                    );
-                  },
+                onPressed: () {
+                  hand = HandType.allHands;
+                  // Navigator.of(context).pushNamed('/home_screen');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => MyHome(hand),
+                    ),
+                  );
+                },
+              ),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'High Count Hands',
+                  style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
                 ),
-                FlatButton(
-                  color: Colors.white,
-                  child: Text(
-                    'Pairs',
-                    style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
-                  ),
-                  onPressed: () {
-                    hand = HandType.pairs;
-                    // Navigator.of(context).pushNamed('/home_screen');
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => MyHome(hand),
-                      ),
-                    );
-                  },
+                onPressed: () {
+                  hand = HandType.allHands;
+                  // Navigator.of(context).pushNamed('/home_screen');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => MyHome(hand),
+                    ),
+                  );
+                },
+              ),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'Countdown a Deck',
+                  style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
                 ),
-                FlatButton(
-                  color: Colors.white,
-                  child: Text(
-                    'All Hands',
-                    style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
-                  ),
-                  onPressed: () {
-                    hand = HandType.allHands;
-                    // Navigator.of(context).pushNamed('/home_screen');
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => MyHome(hand),
-                      ),
-                    );
-                  },
-                ),
-                FlatButton(
-                  color: Colors.white,
-                  child: Text(
-                    'Countdown a Deck',
-                    style: TextStyle(fontSize: 14, fontFamily: 'Verdana'),
-                  ),
-                  onPressed: () {
-                    hand = HandType.allHands;
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => CountDeck(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
+                onPressed: () {
+                  hand = HandType.allHands;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => CountDeck(),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
